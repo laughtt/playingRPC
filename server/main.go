b2b99c64af3a6f1cfd01dd845c71858e81fc064f
laught <jcarpioherrera@gmail.com>
2019-11-27 18:01:44 -0500

    a

--- server/main.go
+++ server/main.go
@@ -3,14 +3,17 @@ package main
 import (
 	"fmt"
 	"log"
-t"
-	"github.com/laughtt/playingRPC/api"
+	"net"
+
+	"gitlab.com/pantomath-io/demo-grpc/api"
+
+	//"github.com/laughtt/playingRPC/api"
 	"google.golang.org/grpc"
 )
 
 func main() {
 	//listener TCP PORT 7777
-	lis, err := net.listen("tcp", fmt.Sprintf(":%d", 7777))
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 7777))
 	if err != nil {
 		log.Fatalf("Failed to listen")
 	}
@@ -22,7 +25,6 @@ func main() {
 
 	//atach the ping service to the server
 	api.RegisterPingServer(grpcServer, &s)
-
 	if err := grpcServer.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %s", err)
 	}
